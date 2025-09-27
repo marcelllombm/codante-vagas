7dc006523a9a7243c9dffada21fa9530a5e20a91
Marcelllombm <marceloborgesdemelo@macminimarcelllombm.local>
2025-09-27 17:15:51 -0300

    reafactor code struture

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,7 +1,19 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="color: black">
-      <h1>Ola mundo do nextjs</h1>
-    </div>
+    <section className="pb-10 text-center">
+      <h1 className="font-display mx-auto w-xl text-5xl font-black">
+        Diga adeus às longas buscas para uma vaga
+      </h1>
+      <Button className="mt-12 cursor-pointer" variant={"outline"}>
+        Busque Uma Vaga
+      </Button>
+      <img
+        src="/people.png"
+        alt="Ilustração de pessoas"
+        className="mx-auto mt-12"
+      />
+    </section>
   );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,12 +3,12 @@ import { cn } from "@/lib/utils";
 
 export default function Notfound() {
   return (
-    <main className="mx-auto max-w-5xl py-10 text-center">
+    <main className="mx-auto max-w-4xl py-10 text-center">
       <h1 className="font-display text-5xl font-bold text-gray-500">
         Nada encontrado aqui
       </h1>
       <img
-        className="mx-auto mt-10 w-lg"
+        className="mx-auto mt-10 w-xs"
         src="/astrounalt.png"
         alt="Image de um barco afundando"
       />
@@ -17,10 +17,7 @@ export default function Notfound() {
       </p>
       <a
         href="/"
-        className={cn(
-          buttonVariants({ variant: "default" }),
-          "mt-12 px-6 py-8 text-lg",
-        )}
+        className={cn(buttonVariants({ variant: "default" }), "mt-6")}
       >
         Voltar ao Home
       </a>
