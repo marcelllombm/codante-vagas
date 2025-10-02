d054e4ffcf6b126c250a4c24de5dabab404d7261
Marcelllombm <marceloborgesdemelo@macminimarcelllombm.local>
2025-10-02 02:17:59 -0300

    feat(vagas) implement job creation functionality in cadastro component

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -12,7 +12,22 @@ export async function deleteJob(formData: FormData) {
   );
 
   if (!res.ok) {
-    throw new Error("algo deu errado ao apagar a vaga");
+    throw new Error("Erro ao deletar vaga");
   }
+  redirect("/vagas");
+}
+
+export async function createJob(formData: FormData) {
+  const res = await fetch("https://apis.codante.io/api/job-board/jobs", {
+    method: "POST",
+    body: formData,
+  });
+
+  if (!res.ok) {
+    throw new Error(
+      `Erro ao inserir nova vaga ${res.status} - ${res.statusText} - ${await res.text()}`,
+    );
+  }
+
   redirect("/vagas");
 }
